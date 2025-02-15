0bc6ed66d0dd67550e629960909e4bd829c35896
kayumansari786 <kayum.ansari@omindtech.com>
2025-02-15 20:55:36 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from './component/Login'
+import './assets/css/style.css'
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App;
