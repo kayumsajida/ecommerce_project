5685149648a352f4bdd40fb4705015349d53ec20
kayumsajida <kayum.cool19@gmail.com>
2025-03-15 20:23:42 +0530

    Responsive added

--- src/component/CategoriesWise.jsx
+++ src/component/CategoriesWise.jsx
@@ -4,7 +4,7 @@ const CategoriesWise = () => {
         <>
             <section className="pt-20">
                 <div className="container mx-auto">
-                    <div className="grid grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
                         <div className="relative group overflow-hidden">
                             <img src="src/assets/images/grid_banner_1.jpg" className="w-full object-cover group-hover:scale-110 transition-all duration-500 rounded-2xl" alt="New Horizon" />
                             <div className="absolute top-1/2 w-full text-center p-3">
@@ -23,8 +23,8 @@ const CategoriesWise = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="bg-[url(src/assets/images/pattern.webp)] bg-cover bg-no-repeat rounded-2xl relative">
-                            <div className="absolute top-1/2 transform -translate-y-9 text-center w-full">
+                        <div className="relative p-10 lg:p-0 bg-[url(src/assets/images/pattern.webp)] bg-cover bg-no-repeat rounded-2xl ml-4 mr-4 lg:ml-0 lg:mr-0">
+                            <div className="lg:absolute lg:top-1/2 lg:transform lg:-translate-y-9 text-center w-full">
                                 <h2 className="text-4xl text-white font-bold">Kids Collection</h2>
                                 <Link to="#" className="text-white mt-3 block font-bold hover:text-red-500 transition-all duration-500">Shop Now</Link>
                             </div>
