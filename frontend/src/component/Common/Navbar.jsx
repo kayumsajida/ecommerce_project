2876c740e2cc73684ef599559cc6af134a48009e
kayumsajida <kayum.cool19@gmail.com>
2025-04-21 18:51:23 +0530

    working on it

--- frontend/src/component/Common/Navbar.jsx
+++ frontend/src/component/Common/Navbar.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X, User, ShoppingCart, Search } from "lucide-react";
 import SearchBar from "./SerachBar";
+import CartDrawer from "../Layout/CartDrawer";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const [cartOpen, setCartOpen] = useState(false);
+
+    const toggleCart = () => {
+        setCartOpen(!cartOpen);
+    }
+
   return (
+    <>
     <nav className="bg-white sticky top-0 z-10">
       <div className="container mx-auto px-4 md:px-6 lg:px-0">
         <div className="flex justify-between items-center py-4">
@@ -26,14 +34,14 @@ const Navbar = () => {
 
           {/* Login/Signup (Always Visible) */}
           <div className="items-center hidden md:flex gap-4">
-            <div className="relative">
+            <button onClick={toggleCart} className="relative cursor-pointer">
               <ShoppingCart className="h-5 w-5" />
               <div className="absolute -top-3 -right-2">
                 <div className="flex w-4 h-4 bg-red-500 rounded-full text-white items-center justify-center text-[10px]">
                   0
                 </div>
               </div>
-            </div>
+            </button>
             <div className="group relative">
               <Link to="#"><User className="h-5 w-5" /></Link>
               <div className="hidden absolute right-0 transform -translate-y-2 group-hover:block transition-all duration-500">
@@ -61,6 +69,40 @@ const Navbar = () => {
           </div>
 
           {/* Mobile Menu Button */}
+          <div className="flex flex-row gap-2 md:hidden">
+              <button onClick={toggleCart} className="relative cursor-pointer">
+                <ShoppingCart className="h-5 w-5" />
+                <div className="absolute -top-3 -right-2">
+                  <div className="flex w-4 h-4 bg-red-500 rounded-full text-white items-center justify-center text-[10px]">
+                    0
+                  </div>
+                </div>
+              </button>
+              <div className="group relative">
+                <Link to="#"><User className="h-5 w-5" /></Link>
+                <div className="hidden absolute right-0 transform -translate-y-2 group-hover:block transition-all duration-500">
+                  <ul className="bg-white p-2 w-40 shadow-lg rounded-lg mt-4">
+                    <li>
+                      <Link to="/login" className="text-sm inline-flex w-full px-2 py-1 transition-all duration-500 hover:bg-gray-50">
+                        Login
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/signup" className="text-sm inline-flex w-full px-2 py-1 transition-all duration-500 hover:bg-gray-50">
+                        Register
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/forgot-password" className="text-sm inline-flex w-full px-2 py-1 transition-all duration-500 hover:bg-gray-50">
+                        Forgot Password
+                      </Link>
+                    </li>
+                  </ul>
+                </div>           
+
+              </div>
+              <SearchBar />
+            </div>
           <button onClick={() => setIsOpen(!isOpen)} className="md:hidden cursor-pointer">
             {isOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
@@ -73,37 +115,14 @@ const Navbar = () => {
             <Link to="#" className="text-neutral-600 hover:text-red-500 transition font-medium">About Us</Link>
             <Link to="#" className="text-neutral-600 hover:text-red-500 transition font-medium">Shop</Link>
             <Link to="#" className="text-neutral-600 hover:text-red-500 transition font-medium">Product</Link>
-            <div className="flex flex-row gap-4">
-              <div className="relative">
-                <ShoppingCart />
-                <div className="absolute left-5 -top-3 -right-2">
-                  <div className="flex w-4 h-4 bg-red-500 rounded-full text-white items-center justify-center text-[10px]">
-                    0
-                  </div>
-                </div>
-              </div>
-              <div className="group relative">
-                <Link to="#"><User /></Link>
-                <div className="hidden absolute left-0 transform -translate-y-2 group-hover:block transition-all duration-500">
-                  <ul className="bg-white p-3 w-40 shadow-lg rounded-lg mt-4">
-                    <li>
-                      <Link to="#" className="text-sm block px-2 py-1 transition-all duration-500 hover:bg-gray-50">Login</Link>
-                    </li>
-                    <li>
-                      <Link to="#" className="text-sm block px-2 py-1 transition-all duration-500 hover:bg-gray-50">Register</Link>
-                    </li>
-                    <li>
-                      <Link to="#" className="text-sm block px-2 py-1 transition-all duration-500 hover:bg-gray-50">Forgot Password</Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <SearchBar />
-            </div>
+            
           </div>
         )}
       </div>
     </nav>
+<CartDrawer cartOpen={cartOpen} toggleCart={toggleCart}  />
+
+    </>
   );
 };
 
