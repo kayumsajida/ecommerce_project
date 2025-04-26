af3284df80ff6bf8a5d366b811c08304f5936b67
kayumsajida <kayum.cool19@gmail.com>
2025-04-26 19:16:34 +0530

    working on product details page

--- frontend/src/component/Pages/Home.jsx
+++ frontend/src/component/Pages/Home.jsx
@@ -3,6 +3,7 @@ import TopTrending from "../Product/TopTrending";
 import FAQ from "../Common/FAQ";
 import Collection from "../Product/Collection";
 import CategoriesWise from "../Product/CategoriesWise";
+import ProductDetails from "../Product/ProductDetails";
 
 const Home = () => {
     return (        
@@ -10,8 +11,10 @@ const Home = () => {
             <Banner />
             <CategoriesWise />                                 
             <TopTrending />
+            <h2 className="text-4xl font-bold">Best Seller</h2>
+            <ProductDetails />
             <FAQ />
-            <Collection />
+            <Collection />            
         </>
     )
 }
